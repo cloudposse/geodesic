6f2d0c578c1f467f3956ebedd9753b6f730374da
Erik Osterman <e@osterman.com>
2017-02-28 13:37:46 -0800

    brandable shell name

---
+++ rootfs/etc/profile.d/iterm.sh
@@ -0,0 +1,15 @@
+set_badge() {
+  if [ "${TERM_PROGRAM}" == "iTerm.app" ]; then
+    printf "\e]1337;SetBadgeFormat=%s\a" $(echo "$1" | base64)
+  fi
+}
+
+## Display an exit greeting
+function _exit() {
+  set_badge ""
+  echo 'Goodbye'
+  exit 0
+}
+trap _exit EXIT
+
+set_badge "${SHELL_NAME}"

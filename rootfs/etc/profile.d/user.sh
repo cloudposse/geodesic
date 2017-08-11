93bf3f9227d1b136ce42ab4a85b9f1c41c72c790
Sergey Vasilyev <s2504s@gmail.com>
2017-08-11 12:13:42 +0300

    Added to ENV uid and gid of current user

--- rootfs/etc/profile.d/user.sh
+++ rootfs/etc/profile.d/user.sh
@@ -2,5 +2,7 @@
 
 id "${USER}" &>/dev/null
 if [[ "$?" -ne 0 ]]; then
-    adduser -D -H ${USER} &>/dev/null
+    if [[ -n "${USER_ID}" ]] && [[ -n "${GROUP_ID}" ]]; then
+        adduser -D -u ${USER_ID} -g ${GROUP_ID} -h ${HOME} ${USER} &>/dev/null
+    fi
 fi

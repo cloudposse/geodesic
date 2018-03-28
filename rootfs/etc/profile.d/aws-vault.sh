0838ffb477ee2753c0255ab67bded2c3bbf423dd
Erik Osterman <erik@cloudposse.com>
2018-03-28 00:19:43 -0700

    reorg assume-role

--- rootfs/etc/profile.d/aws-vault.sh
+++ rootfs/etc/profile.d/aws-vault.sh
@@ -2,3 +2,20 @@
 
 [ -d /localhost/.awsvault ] || mkdir /localhost/.awsvault
 ln -sf /localhost/.awsvault ${HOME}
+
+# Alias to start a shell or run a command with an assumed role
+function assume-role() {
+  role=$1
+  shift
+  if [ $# -eq 0 ]; then
+    aws-vault exec $role bash
+  else
+    aws-vault exec $role $*
+  fi
+}
+
+# Alias for backwards compatbility
+function use-profile() {
+  assume-role $*
+}
+
--- rootfs/etc/profile.d/geodesic.sh
+++ rootfs/etc/profile.d/geodesic.sh
@@ -1,17 +1,2 @@
 export GEODESIC_SHELL=true
-export SSH_AGENT_CONFIG=${LOCAL_STATE}/.ssh-agent
-
-function assume-role() {
-  role=$1
-  shift
-  if [ $# -eq 0 ]; then
-    aws-vault exec $role bash
-  else
-    aws-vault exec $role $*
-  fi
-}
-
-function use-profile() {
-  assume-role $*
-}
-
+export SSH_AGENT_CONFIG=/var/tmp/.ssh-agent

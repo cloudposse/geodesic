b7a833611ea6a24622df8c95bb709aebe1564a0d
Erik Osterman <e@osterman.com>
2018-08-01 22:42:21 -0700

    Restore white checkmark

--- rootfs/etc/profile.d/prompt.sh
+++ rootfs/etc/profile.d/prompt.sh
@@ -39,7 +39,7 @@ function geodesic_prompt() {
       ;;
     *)
       # unicode
-      AWS_VAULT_ACTIVE_MARK=$'\u2714 '      # '✔'
+      AWS_VAULT_ACTIVE_MARK=$'\u2705 '      # '✅'
       AWS_VAULT_INACTIVE_MARK=$'\u274C '    # '❌'
       BLACK_RIGHTWARDS_ARROWHEAD=$'\u27A4 ' # '➤', suggest '▶' may be present in more fonts
       BANNER_MARK=$'\u29C9 '                # '⧉'

27e2177c96fd926d8b742f27bb4dd345c17fb065
Erik Osterman <erik@cloudposse.com>
2018-08-17 10:29:57 -0700

    Colorize messages (#221)

---
+++ rootfs/etc/profile.d/_colors.sh
@@ -0,0 +1,15 @@
+function red() {
+  echo "$(tput setaf 1)$*$(tput sgr0)"
+}
+
+function green() {
+  echo "$(tput setaf 2)$*$(tput sgr0)"
+}
+
+function yellow() {
+  echo "$(tput setaf 3)$*$(tput sgr0)"
+}
+
+function cyan() {
+  echo "$(tput setaf 6)$*$(tput sgr0)"
+}

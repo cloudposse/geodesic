557117e9a1bbe684c12249ace98829762c2778db
Marc Tamsky <tamsky@users.noreply.github.com>
2019-02-27 08:53:22 -0800

    shfmt

--- rootfs/etc/profile.d/inputrc.sh
+++ rootfs/etc/profile.d/inputrc.sh
@@ -1,7 +1,7 @@
 # If we can, and do not have one, copy inputrc from localhost.
 [ -f ~/.inputrc ] && [ -f /localhost/.inputrc ] || {
-    cp /localhost/.inputrc ~/.inputrc &&
-        # Use new bindings for current shell.
-        bind -f ~/.inputrc ;
+	cp /localhost/.inputrc ~/.inputrc &&
+		# Use new bindings for current shell.
+		bind -f ~/.inputrc
 }
 # Subsequent shells automatically read ${INPUTRC-"~/.inputrc"} at startup.

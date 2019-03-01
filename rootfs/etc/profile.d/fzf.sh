07617250133a2bb7ba3e974011bc104a60be936c
Erik Osterman <erik@cloudposse.com>
2019-02-28 23:26:28 -0800

    Fix formating

--- rootfs/etc/profile.d/fzf.sh
+++ rootfs/etc/profile.d/fzf.sh
@@ -1,7 +1,5 @@
-
 # Install fzf shell completion when an interactive shell is enabled
 # This is a fix for: `/etc/bash_completion.d/fzf.sh: line 34: bind: warning: line editing not enabled`
 if [ -t 1 ]; then
 	ln -s /usr/share/bash-completion/completions/fzf /etc/bash_completion.d/fzf.sh
 fi
-

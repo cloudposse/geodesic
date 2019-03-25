9341c3c6f075af1c7e1aae00fdd4b1948564966c
Nuru <Nuru@users.noreply.github.com>
2019-03-24 23:29:21 -0700

    fmt

--- rootfs/etc/profile.d/tfenv.sh
+++ rootfs/etc/profile.d/tfenv.sh
@@ -1,7 +1,8 @@
 # Automatically export the current environment to `TF_VAR`
 # Use a regex defined in the `TFENV_WHITELIST` and `TFENV_BLACKLIST` environment variables to include and exclude variables
 #
-[[ ${LOAD_GLOBAL_TFENV:-false} == "true" ]] &&
+if [[ ${TFENV_LOAD_GLOBALLY:-false} == "true" ]]; then
 	echo $(red Global use of) tfenv $(red is not recommended) &&
-	echo $(red use the) '"use tfenv"' $(red function of) '"direnv"' $(red to automatically execute it in certain directories) &&
-	source <(tfenv)
+		echo $(red use the) '"use tfenv"' $(red function of) '"direnv"' $(red to automatically execute it in certain directories) &&
+		source <(tfenv)
+fi

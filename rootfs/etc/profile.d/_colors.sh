ca6e9136b056771a3715c05dcee37c34935595b6
Nuru <Nuru@users.noreply.github.com>
2019-03-24 17:20:19 -0700

    Provide a variety of color palettes for `fzf`, selectable via FZF_COLORS

--- rootfs/etc/profile.d/_colors.sh
+++ rootfs/etc/profile.d/_colors.sh
@@ -14,8 +14,3 @@ function cyan() {
 	echo "$(tput setaf 6)$*$(tput sgr0)"
 }
 
-export FZF_DEFAULT_OPTS='
-  --color=bg+:#073642,bg:#002b36,spinner:#719e07,hl:#586e75
-  --color=fg:#839496,header:#586e75,info:#cb4b16,pointer:#719e07
-  --color=marker:#719e07,fg+:#839496,prompt:#719e07,hl+:#719e07
-'

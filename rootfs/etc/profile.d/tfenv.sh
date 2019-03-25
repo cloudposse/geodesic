f7f7f9d9dfa382a3f497ff9fc57be3d6cee447d1
Nuru <Nuru@users.noreply.github.com>
2019-03-24 17:24:09 -0700

    Disable gobal `tfenv` by default but allow it to be turned back on  via configuration

--- rootfs/etc/profile.d/tfenv.sh
+++ rootfs/etc/profile.d/tfenv.sh
@@ -1,3 +1,7 @@
 # Automatically export the current environment to `TF_VAR`
 # Use a regex defined in the `TFENV_WHITELIST` and `TFENV_BLACKLIST` environment variables to include and exclude variables
-source <(tfenv)
+#
+[[ ${LOAD_GLOBAL_TFENV:-false} == "true" ]] && \
+ echo $(red Global use of ) tfenv $(red is not recommended) && \
+ echo $(red use the) '"use tfenv"' $(red function of) '"direnv"' $(red to automatically execute it in certain directories) && \
+ source <(tfenv)

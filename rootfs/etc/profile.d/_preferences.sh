8e1c3a1c886aa86de1375ef9d16c6450c198fb39
Nuru <Nuru@users.noreply.github.com>
2019-03-28 17:09:24 -0700

    [history] Fix bash history file initialization (#428)

--- rootfs/etc/profile.d/_preferences.sh
+++ rootfs/etc/profile.d/_preferences.sh
@@ -46,9 +46,10 @@ unset _GEODESIC_CONFIG_HOME_DEFAULT
 # Search for and find the history file most specificly targeted to this DOCKER_IMAGE
 function _geodesic_set_histfile() {
 	## Save shell history in the most specific place
+	local histfile_at_start="${HISTFILE}"
 	local histfile_list=(${HISTFILE:-${GEODESIC_CONFIG_HOME}/history})
-	_search_geodesic_dirs HISTFILE_LIST history
-	HISTFILE="${histfile_list[-1]}"
+	_search_geodesic_dirs histfile_list history
+	export HISTFILE="${histfile_list[-1]}"
 	[[ -n $_GEODESIC_TRACE_CUSTOMIZATION ]] && echo trace: HISTFILE set to "${HISTFILE}"
 }
 _geodesic_set_histfile

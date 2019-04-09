d49881407edbae42fec6e3fd67e8b8209604be54
Erik Osterman <erik@cloudposse.com>
2019-04-08 23:26:34 -0700

    Squelch "Goodbye" if not interactive

--- rootfs/etc/profile.d/iterm.sh
+++ rootfs/etc/profile.d/iterm.sh
@@ -1,5 +1,7 @@
+# A badge is a large text label that appears in the top right of a terminal session to provide dynamic status
 set_badge() {
 	if [ "${TERM_PROGRAM}" == "iTerm.app" ]; then
+		# https://www.iterm2.com/documentation-badges.html
 		printf "\e]1337;SetBadgeFormat=%s\a" $(echo "$1" | base64)
 	fi
 }
@@ -11,6 +13,9 @@ function _exit() {
 	echo 'Goodbye'
 	exit $status
 }
-trap _exit EXIT
 
-set_badge "${SHELL_NAME}"
+# Friendly exit greeting for interactive terminals
+if [ -t 1 ]; then
+	trap _exit EXIT
+	set_badge "${SHELL_NAME}"
+fi

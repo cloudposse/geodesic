07d00e81946ecc35b39c8ea9ec372ab5a83df0db
Nuru <Nuru@users.noreply.github.com>
2019-04-14 17:12:50 -0700

    Fix bug preventing role-server from being run more than once

--- rootfs/etc/profile.d/aws-vault.sh
+++ rootfs/etc/profile.d/aws-vault.sh
@@ -206,7 +206,7 @@ if [ "${AWS_VAULT_ENABLED:-true}" == "true" ]; then
 			exit 99
 		fi
 
-		AWS_VAULT_ARGS+=("--debug")
+		[[ ${AWS_VAULT_ARGS[*]} =~ --debug ]] || AWS_VAULT_ARGS+=("--debug")
 		_aws_vault_assume_role "${1:-$(choose_role)}" sleep 7d
 	}
 

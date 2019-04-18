d54254d03a5593ee090fa9cbe40be45e6015d91c
Nuru <Nuru@users.noreply.github.com>
2019-04-18 16:51:53 -0700

    Set env vars for tsh

--- rootfs/etc/profile.d/tsh-login.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-function tsh-login() {
-	if (($# == 0)); then
-		tsh login --bind-addr=:${GEODESIC_PORT} --proxy=${TELEPORT_LOGIN_PROXY:-tele.${DOCKER_IMAGE#*.}} $STAGE
-	else
-		tsh login --bind-addr=:${GEODESIC_PORT} --proxy=${TELEPORT_LOGIN_PROXY:-tele.${DOCKER_IMAGE#*.}} $*
-	fi
-}

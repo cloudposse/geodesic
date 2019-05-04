35098023f20d152fa8a259968300790235a78221
Erik Osterman <erik@cloudposse.com>
2019-05-03 18:42:20 -0700

    fix formatting

--- rootfs/etc/profile.d/help.sh
+++ rootfs/etc/profile.d/help.sh
@@ -3,7 +3,8 @@ function help() {
 	if [ $# -ne 0 ]; then
 		apropos "$*"
 	else
-		echo -e "Available documentation:\n\n"
+		echo -e "Available documentation:\n"
 		apropos . | sed 's/^/  /'
+		echo
 	fi
 }

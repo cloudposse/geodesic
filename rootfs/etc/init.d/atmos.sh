01704b6b08ed2a3b01791c475cc85db9924916b6
nitrocode <nitrocode@users.noreply.github.com>
2022-01-12 14:13:22 -0500

    Check if ATMOS_BASE_PATH is unset before writing over it

--- rootfs/etc/init.d/atmos.sh
+++ rootfs/etc/init.d/atmos.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-if [ -d "${GEODESIC_WORKDIR}/stacks" -a -d "${GEODESIC_WORKDIR}/components" ]; then
+# check if atmos base path is unset and verify that the stacks and components dir is in current directory
+if [ -z $ATMOS_BASE_PATH ] && [ -d "${GEODESIC_WORKDIR}/stacks" -a -d "${GEODESIC_WORKDIR}/components" ]; then
   export ATMOS_BASE_PATH=${GEODESIC_WORKDIR}
+  echo "Set ATMOS_BASE_PATH = ${GEODESIC_WORKDIR}"
 fi

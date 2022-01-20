10238081212fbe63a9c7b7bdbe70731aaa0e7ac8
nitrocode <nitrocode@users.noreply.github.com>
2022-01-20 12:49:46 -0500

    WIP attempt to fix atmos.sh init script

--- rootfs/etc/init.d/atmos.sh
+++ rootfs/etc/init.d/atmos.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 # check if atmos base path is unset and verify that the stacks and components dir is in current directory
-if [ -z $ATMOS_BASE_PATH ] && [ -d "${GEODESIC_WORKDIR}/stacks" -a -d "${GEODESIC_WORKDIR}/components" ]; then
-  export ATMOS_BASE_PATH=${GEODESIC_WORKDIR}
-  echo "Set ATMOS_BASE_PATH = ${GEODESIC_WORKDIR}"
-fi
+function atmos-base {
+  local PATH=${1:-$PWD}
+  if [ -z $ATMOS_BASE_PATH ] && [ -d "$PATH/stacks" -a -d "$PATH/components" ]; then
+    # echo "Set ATMOS_BASE_PATH = ${PATH}"
+    export ATMOS_BASE_PATH="$PATH"
+  # else
+    # echo "Cannot find stacks/ and components/ in $PATH"
+  fi
+}
+
+atmos-base

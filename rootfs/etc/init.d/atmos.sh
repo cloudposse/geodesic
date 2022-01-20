58e7c24269ef86f21fcf0d8b6996c534a4a2a61c
nitrocode <nitrocode@users.noreply.github.com>
2022-01-20 13:33:37 -0500

    Quote strings

--- rootfs/etc/init.d/atmos.sh
+++ rootfs/etc/init.d/atmos.sh
@@ -2,8 +2,8 @@
 
 # check if atmos base path is unset and verify that the stacks and components dir is in current directory
 function atmos-base {
-  local PATH=${1:-$PWD}
-  if [ -z $ATMOS_BASE_PATH ] && [ -d "$PATH/stacks" -a -d "$PATH/components" ]; then
+  local PATH="${1:-$PWD}"
+  if [ -z "$ATMOS_BASE_PATH" ] && [ -d "$PATH/stacks" -a -d "$PATH/components" ]; then
     # echo "Set ATMOS_BASE_PATH = ${PATH}"
     export ATMOS_BASE_PATH="$PATH"
   # else

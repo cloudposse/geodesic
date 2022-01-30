e8d83fecccc9d2d35c39db3bb8343aea1e68d381
Mike Crowe <drmikecrowe@gmail.com>
2022-01-30 00:17:49 -0500

    Add support for Linux users and custom install script

--- rootfs/etc/profile.d/syslog-ng.sh
+++ rootfs/etc/profile.d/syslog-ng.sh
@@ -1,3 +1,9 @@
 if ! pidof syslog-ng >/dev/null; then
-	syslog-ng -f /etc/syslog-ng/syslog-ng.conf --no-caps
+    SUDO=""
+    if [ $(id -u) != 0 ]; then
+        # If we are running as a user, sbin isn't in the path and we need to start via sudo
+        PATH=/usr/sbin:/usr/bin:/bin:/sbin
+        SUDO="sudo"
+    fi
+    $SUDO syslog-ng -f /etc/syslog-ng/syslog-ng.conf --no-caps
 fi

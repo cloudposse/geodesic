c247118b168dfe704df60debda2d3cea6b894f6e
Nuru <Nuru@users.noreply.github.com>
2022-02-03 00:38:46 -0800

    Use bindfs to preserve host file ownership

--- rootfs/etc/profile.d/_40-preferences.sh
+++ rootfs/etc/profile.d/_40-preferences.sh
@@ -1,7 +1,8 @@
 # Files in the profile.d directory are executed by the lexicographical order of their file names.
-# This file is named _preferences.sh. The leading underscore is needed to ensure this file executes before
-# other files that depend on the functions defined here.
-# This file has depends on _geodesic-config.sh and should come third.
+# This file is named _40-preferences.sh. The leading underscore is needed to ensure this file
+# executes before other files that depend on the functions defined here.
+# The number portion is to ensure proper ordering among the high-priority scripts.
+# This file has depends on colors.sh, geodesic-config.sh, and localhost.sh and should come after them.
 # This file loads user preferences/customizations and must load before any user-visible configuration takes place.
 
 # In case this output is being piped into a shell, print a warning message
@@ -58,10 +59,10 @@ elif [[ ! -d $GEODESIC_CONFIG_HOME ]]; then
 fi
 
 if [[ ! -d $GEODESIC_CONFIG_HOME ]]; then
-	if ! df | grep -q /localhost; then
+	if ! df -a | grep -q " ${GEODESIC_LOCALHOST:-/localhost}\$"; then
 		if [[ -z $KUBERNETES_PORT ]]; then
 			red "########################################################################################" >&2
-			red "# No filesystem is mounted at $(bold /localhost) which limits Geodesic functionality." >&2
+			red "# No filesystem is mounted at $(bold ${GEODESIC_LOCALHOST:-/localhost}) which limits Geodesic functionality." >&2
 			boot install
 		else
 			echo $(green Kubernetes host detected, Geodesic customization disabled.)
--- rootfs/etc/profile.d/_50-workdir.sh
+++ rootfs/etc/profile.d/_50-workdir.sh
@@ -1,9 +1,8 @@
 # Files in the profile.d directory are executed by the lexicographical order of their file names.
-# This file sets the working directory inside Geodesic to match the host directory Geodesic
-# was launched from, if possible. If the host directory is not accessible, it sets the working directory to `/`.
-#
-# This file is named _workdir.sh. The leading underscore is needed to ensure this file executes before
-# other files that may depend on it. The "w" is needed to ensure it is loaded *after* _preferences.sh
+# This file is named _50-workdir.sh. The leading underscore is needed to ensure this file
+# executes before other files that may depend on it.
+# The number portion is to ensure proper ordering among the high-priority scripts.
+# This file depends on colors.sh, localhost.sh, and preferences,sh and must come after them
 #
 
 function _file_device() {
@@ -16,8 +15,8 @@ function _file_device() {
 # Therefore we cache some info in the environment.
 if [[ $GEODESIC_LOCALHOST_DEVICE == "disabled" ]]; then
 	red "# Host filesystem device detection disabled."
-elif df -a | grep -q /localhost; then
-	export GEODESIC_LOCALHOST_DEVICE=$(_file_device /localhost)
+elif df -a | grep -q " ${GEODESIC_LOCALHOST:-/localhost}\$"; then
+	export GEODESIC_LOCALHOST_DEVICE=$(_file_device "${GEODESIC_LOCALHOST:-/localhost}")
 	if [[ $GEODESIC_LOCALHOST_DEVICE == $(_file_device /) ]]; then
 		red "# Host filesystem device detection failed. Falling back to \"path starts with /localhost\"."
 		GEODESIC_LOCALHOST_DEVICE="same-as-root"
@@ -32,7 +31,8 @@ function file_on_host() {
 	elif [[ $GEODESIC_LOCALHOST_DEVICE == "same-as-root" ]]; then
 		[[ $(readlink -e "$1") =~ ^/localhost(/.*)?$ ]]
 	else
-		[[ $(_file_device "$1") == ${GEODESIC_LOCALHOST_DEVICE} ]]
+		local regex="^(${GEODESIC_LOCALHOST_DEVICE}${GEODESIC_LOCALHOST_MAPPED_DEVICE:+|${GEODESIC_LOCALHOST_MAPPED_DEVICE}})\$"
+		[[ $(_file_device "$1") =~ ${regex} ]]
 	fi
 }
 
@@ -52,7 +52,7 @@ if [[ -d $GEODESIC_WORKDIR ]]; then
 	[[ $SHLVL == 1 ]] && green "# Initial working directory configured as ${GEODESIC_WORKDIR}"
 else
 	if [[ -d $GEODESIC_HOST_CWD ]]; then
-		if [[ -n $LOCAL_HOME ]] && { [[ $GEODESIC_LOCALHOST_DEVICE  == "disabled" ]] || $(file_on_host "$GEODESIC_HOST_CWD"); }; then
+		if [[ -n $LOCAL_HOME ]] && { [[ $GEODESIC_LOCALHOST_DEVICE  == "disabled" ]] || file_on_host "$GEODESIC_HOST_CWD"; }; then
 			export GEODESIC_WORKDIR=$(readlink -e "${GEODESIC_HOST_CWD}")
 			green "# Initial working directory set from host CWD to ${GEODESIC_WORKDIR}"
 		else

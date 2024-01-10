a0420acecdf62bebd48b5a4506d1cb9a38239a17
Mark Ferrell <mark.ferrell@aplaceformom.com>
2024-01-10 14:56:04 -0800

    fix: handling of user+group creation on Debian
    The adduser/addgroup commands on Debian based systems are much more
    pedantic than other platforms and will error out on some surprising
    conditions. Further more, we want to support situations in which the
    username or groupname is pulled from systems which support characters
    such a spaces in the names which are prohibited in the POSIX
    passwd/group files.

--- rootfs/etc/profile.d/user.sh
+++ rootfs/etc/profile.d/user.sh
@@ -1,8 +1,20 @@
 #!/usr/bin/env bash
 
-id "${USER}" &>/dev/null
-if [[ "$?" -ne 0 ]]; then
+if [[ -n "${GROUP}" ]] && ! id -g "${GROUP}" &>/dev/null; then
+	if [[ -n "${GROUP_ID}" ]]; then
+		addgroup --force-badname --gid "${GROUP_ID}" "${GROUP}" &>/dev/null
+	fi
+fi
+
+if ! id "${USER}" &>/dev/null; then
 	if [[ -n "${USER_ID}" ]] && [[ -n "${GROUP_ID}" ]]; then
-		adduser -D -u ${USER_ID} -g ${GROUP_ID} -h ${HOME} ${USER} &>/dev/null
+		if [[ "${GEODESIC_OS}" = 'debian' ]]; then
+			# Trust the host USER a much as permissible, to that end we need to force
+			# a bad username for cases in which the username may contain dots and the
+			# like.
+			adduser --force-badname --uid "${USER_ID}" --gid "${GROUP_ID}" --home "${HOME}" --disabled-password --gecos '' "${USER}" &>/dev/null
+		else
+			adduser -D -u "${USER_ID}" -g "${GROUP_ID}" -h "${HOME}" "${USER}" &>/dev/null
+		fi
 	fi
 fi

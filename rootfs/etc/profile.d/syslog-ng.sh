007e3378da48b05eb747b17e56ab4fae1eca0529
Michael Pursifull <mike@arcaven.com>
2024-02-11 02:06:32 -0600

    Update to Debian 12.4 Bookworm, make minor changes related to the upgrade

--- rootfs/etc/profile.d/syslog-ng.sh
+++ rootfs/etc/profile.d/syslog-ng.sh
@@ -1,3 +1,13 @@
-if ! pidof syslog-ng >/dev/null; then
-	syslog-ng -f /etc/syslog-ng/syslog-ng.conf --no-caps
+# Determine the command to use for checking if syslog-ng is running
+if [ -f "/etc/alpine-release" ]; then
+    # Alpine Linux
+    SYSLOG_NG_PID="pidof syslog-ng"
+else
+    # Assumes Debian or other distributions
+    SYSLOG_NG_PID="pgrep -x syslog-ng"
+fi
+
+# Check if syslog-ng is running and start it if not
+if ! $SYSLOG_NG_PID >/dev/null; then
+    syslog-ng -f /etc/syslog-ng/syslog-ng.conf --no-caps
 fi

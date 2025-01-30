0a9a649743e7e4ddd85f5148dd4bb2599b6f409e
Nuru <Nuru@users.noreply.github.com>
2025-01-30 12:48:38 -0800

    Remove Leapp from MOTD, remove remote MOTD

--- rootfs/etc/motd.sh
+++ rootfs/etc/motd.sh
@@ -8,8 +8,6 @@ IMPORTANT:
 #  * under both \`${LOCAL_HOME}\` and \`${HOME}\`.
 #  * Your AWS configuration should be available at \`${AWS_CONFIG_FILE}\`.
 #  * Your host AWS credentials should be available.
-#  * Use Leapp on your host computer to manage your credentials.
-#  * Leapp is free, open source, and available from https://leapp.cloud
 #  * Use the AWS_PROFILE environment variable to manage your AWS IAM role, or
 #  * you can interactively select AWS profiles via the \`assume-role\` command,
 #  * which will launch a subshell with your selected profile set.
